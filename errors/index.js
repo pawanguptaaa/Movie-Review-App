a6803e346e967fe171c5cb44ce7496f713c38ab0
Pawan Gupta <98523729+pawanguptaaa@users.noreply.github.com>
2022-02-17 15:28:03 +0530

    new updates

---
+++ errors/index.js
@@ -0,0 +1,12 @@
+const BadRequest = require('./BadRequest');
+const CustomError = require('./CustomError');
+const STATUS_CODES = require('./StatusCodes');
+const UnAuthenticatedError=require('./UnAuthenticatedError')
+const  UnAuthorisedError=require('./UnAuthorisedError')
+module.exports = {
+    BadRequest,
+    CustomError,
+    STATUS_CODES,
+    UnAuthenticatedError,
+    UnAuthorisedError
+};
