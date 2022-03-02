d082a0cd3615a259f9d7e0213b751d7038d3447c
Pawan Gupta <98523729+pawanguptaaa@users.noreply.github.com>
2022-03-02 15:22:09 +0530

    updated

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const express = require('express');
-const req = require('express/lib/request');
-const app = express();
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+require('./db/mongoose');
+const customErrorHandler = require('./middleware/errorController');
 
-dotenv.config();
 const authRoute = require('./routes/auth');
 const movieRoute = require('./routes/movies');
-const userroutes=require('./routes/users')
+const userRoute = require('./routes/users');
+const reviewRoute = require('./routes/review');
+const ratingRoute = require('./routes/rating');
+const { BadRequest } = require('./errors');
 
+const app = express();
+app.use(express.json({ limit: '5mb' }));
 
-mongoose.connect(process.env.DB_CONNECT,{ useNewUrlParser: true},() => console.log('Connected to db!'));
-
-
-app.use(express.json());
-
-app.use('/api/user', authRoute);
+app.use('/api/auth', authRoute);
 app.use('/api/movies', movieRoute);
-app.use('/api/usersinfo',userroutes);
+app.use('/api/users', userRoute);
+app.use('/api/reviews', reviewRoute);
+app.use('/api/ratings', ratingRoute);
 
-app.listen(3000, () => console.log('Server up and running'));
+app.all('*', (req, res, next) => {
+  throw new BadRequest(`Requested url ${req.path} not found`);
+});
 
+app.use(customErrorHandler);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server Running at ${port}`);
+});
